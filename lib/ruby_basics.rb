f4b936949f93cf585cfed6066e17510dff27a4be
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-22 14:17:57 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "John"
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(greeting, name)
+puts "#{greeting}#{name}"
 end
 
-def return_a_value
-
+def return_a_value(value)
+puts "#{value}"
 end
 
 def last_evaluated_value
